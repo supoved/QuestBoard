b5ad4984402e1ccab7ddb8df7262ac94c6c2487e
Pavel Luzhetskiy <supoved@gmail.com>
2015-09-03 20:21:38 +0100

    added list control for active quests

--- app/src/main/java/com/supoved/questboard/android/fragments/ActiveQuestsFragment.java
+++ app/src/main/java/com/supoved/questboard/android/fragments/ActiveQuestsFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.supoved.questboard.R;
+import com.supoved.questboard.android.adapters.QuestArrayAdapter;
+import com.supoved.questboard.data.QuestStorage;
+import com.supoved.questboard.data.models.Quest;
 
 public class ActiveQuestsFragment extends Fragment {
     @Override
@@ -16,6 +20,16 @@ public class ActiveQuestsFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.quests_active_fragment, container, false);
+
+        this.init(view);
+
         return view;
+    }
+
+    private void init(View view) {
+        Quest[] quests = new QuestStorage().getActiveQuests();
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        listView.setAdapter(new QuestArrayAdapter(view.getContext(), quests));
     }
 }
